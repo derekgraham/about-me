54525fb64c50f45ff518d90f87068f0fa1958f08
Derek Graham <derekgraham@mac.com>
2016-04-12 15:49:26 -0700

    Base script working

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
-alert('Hi, let\'s play a guessing game about me!');
+alert('Hi, let\'s play a guessing game about me! You have 5 questions to answer.');
+var myPrompts = ['Q1','Q2','Q3','Q4','Q5'];
+var myAnswers = ['YES','NO','YES','YES','NO'];
+var correctAnswers = 0;
 
-var answer1 = prompt('Does Derek have any cats?');
+for (i = 0; i < myPrompts.length; i++)
+{
+  var answer1 = prompt(myPrompts[i]);
 
-if (answer1.toUpper()[0] === 'Y') {
-  alert('boom');
-} else {
-  alert('wrong');
+  if (answer1.toUpperCase() === myAnswers[i][0] || answer1.toUpperCase() === myAnswers[i]) {
+    console.log('The user response to: ' + myPrompts[i] + ' is ' + answer1);
+    correctAnswers ++;
+  }
+  else {
+    alert('Sorry, that was incorrect!');
+  }
+
 }
+  alert('You answered ' + correctAnswers + ' questions correctly!');
