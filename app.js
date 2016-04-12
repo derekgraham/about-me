8a8fd0fa4f94e2d94ffbd953cbb01e71773c7ced
Derek Graham <derekgraham@mac.com>
2016-04-12 13:19:37 -0700

    first updates

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-alert('hi ya\'ll');
+alert('Hi, let\'s play a guessing game about me!');
+
+var answer1 = prompt('Does Derek have any cats?');
+
+if (answer1.toUpper()[0] === 'Y') {
+  alert('boom');
+} else {
+  alert('wrong');
+}
