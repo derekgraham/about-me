86d420ff6c6c3f99b4a44543b7f6b7229d13d064
Derek Graham <derekgraham@mac.com>
2016-04-13 14:14:28 -0700

    added new question prompt and request for user name

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
-alert('Hi, let\'s play a guessing game about me! You have 5 questions to answer.');
+//about-me guessing game
+
+//variable declaration
 var myPrompts = ['Was I born in Canada - Yes or No?',
 'Do I like to bake cookies - Yes or No?',
 'Did I have a pet pig - Yes or No?',
 'Did I work at a fruit cannery - Yes or No?',
-'Do I currently live in Portland, Oregon - Yes or No?'];
-var myAnswers = ['YES','NO','YES','YES','NO'];
+'Do I currently live in Portland, Oregon - Yes or No?',
+'How many miles is it to bike from my house to CodeFellows? Enter a number between 1 and 10.'];
+var myAnswers = ['YES','NO','YES','YES','NO',6];
 var correctAnswers = 0;
+
+// let's start here, asking for user input.
+var userName = prompt('Hello, what is your name?');
+alert('Hi, ' + userName + ' let\'s play a guessing game about me! You have 6 questions to answer.');
 
 for (i = 0; i < myPrompts.length; i++)
 {
   var answer1 = prompt(myPrompts[i]);
   if (answer1 === null) {
-    i = myPrompts.length + 1;
+    break;
   }
   else if (answer1.toUpperCase() === myAnswers[i][0] || answer1.toUpperCase() === myAnswers[i]) {
     console.log('The user response to: ' + myPrompts[i] + ' is \"' + answer1 + '\" -- CORRECT!');
@@ -23,4 +30,4 @@ for (i = 0; i < myPrompts.length; i++)
   }
 
 }
-alert('Thanks for playing! You answered ' + correctAnswers + ' questions correctly!');
+alert('Thanks for playing! You answered ' + correctAnswers + 'out of ' + myPrompts.length + ' questions correctly!');
